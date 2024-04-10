1d17edc245b82aed1805107d764de468601c4aae
h3nse <henrikf.bj@gmail.com>
2024-04-10 10:00:37 +0200

    Create customized system message

--- backend/backend.py
+++ backend/backend.py
@@ -1,4 +1,33 @@
+from openai import OpenAI
+
+client = OpenAI()
+
+# The AI's chat history are stored here
+messages = []
+
+
 def start_simulation(actions, ais):
-    # Create AI's
-    # Run AI's with starting prompt
+    startingPrompt = f"""You enter a small party. 
+                    Pick one of the following actions, formatted as valid JSON in the form \"action\": [your chosen action]
+                    
+                    Available actions:
+                    {actions}"""
+
+    # Add the customized system message for each ai
+    for ai in ais:
+        systemMessage = f"""You are roleplaying as a character named {ai['name']} with these traits:
+
+                        Appearance:
+                        {ai['appearance']}
+
+                        Personality:
+                        {ai['personality']}"""
+
+        messages.append([{"role": "system", "content": systemMessage}])
+
+    run_ais()
+
+
+def run_ais():
+
     pass
